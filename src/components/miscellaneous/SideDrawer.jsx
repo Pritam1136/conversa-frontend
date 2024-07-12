6a869872585df1409e5a58438c772508fde60c5d
Pritam Roy <pritamroy1136@gmail.com>
2024-07-12 18:20:34 +0530

    let's see

--- src/components/miscellaneous/SideDrawer.jsx
+++ src/components/miscellaneous/SideDrawer.jsx
@@ -146,7 +146,7 @@ function SideDrawer() {
           </Button>
         </Tooltip>
         <Text fontSize={"2xl"} fontFamily={"Work sans"}>
-          Conversa
+          GhostText
         </Text>
         <div>
           <Menu>
